5d30b5377345c9e202f64f9badae42817a503216
unknown <Nathan.Beier@outlook.com>
2018-02-25 13:08:43 -0600

    Init movie.js, npm prop-types, set default props, and static propTypes.

--- src/movie.js
+++ src/movie.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default class Movie extends Component{
+export default class Movie extends Component {
+    static propTypes = {
+        movie: PropTypes.shape({
+            title: PropTypes.string.isRequired,
+        }),
+        desc: PropTypes.string
+    }
+
+    static defaultProps = {
+        desc: "Description is not available"
+    }
+
     render() {
         return (
             <div>
-            {this.props.movie.title}
-          </div>
+                <h3>{this.props.movie.title}</h3>
+                <p>{this.props.desc}</p>
+            </div>
         )
     }
 }
