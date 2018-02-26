59a88b0b4f38ceba8a21ef2e2c0a8da9ab1bb2e9
unknown <Nathan.Beier@outlook.com>
2018-02-25 20:54:57 -0600

    Turned Movie.js into functional stateless component. Edited to please eslint.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
+/* eslint react/no-did-mount-set-state: 0 */
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Movie from './movie';
+import Movie from './Movie';
 
-class App extends Component{
-
-state = {
-  movies: []
-}
+class App extends Component {
+  state = {
+    movies: [],
+  }
 
   async componentDidMount() {
     try {
       const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=092ce31322e8209f8d97d06a1dedf7d5&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
       const movies = await res.json();
       this.setState({
-        movies: movies.results
-      })
-    } catch(e) {
+        movies: movies.results,
+      });
+    } catch (e) {
       console.log(e);
     }
   }
 
-   render() {
+  render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -33,6 +33,5 @@ state = {
     );
   }
 }
-
 
 export default App;
